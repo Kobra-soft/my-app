9da61dd01b15f45eb623ce038740f3426d931350
kobra-soft <code.softwaredev@gmail.com>
2025-03-14 22:26:29 +0000

    Commit

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* <Text>Testing CSS</Text> */}
+      <Text>Testing SDK53 BETA Version - Canary!</Text>
+      <Text>Testing CSS</Text>
       <StatusBar style="auto" />
     </View>
   );
