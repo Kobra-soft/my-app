6f341e3dd9d0cb044c4ecf9157cc915d5eb7d217
kobra-soft <code.softwaredev@gmail.com>
2025-03-31 23:47:38 +0100

    Commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing SDK53 BETA Version - Canary!</Text>
+      {/* <Text>Testing SDK53 BETA Version - Canary!</Text> */}
+      <Text>Testing on Emulators! iOS / Android</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#121212",
     alignItems: "center",
     justifyContent: "center",
   },
